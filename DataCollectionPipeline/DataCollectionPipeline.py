4f67f89d031ed00635e97686f194c317cd526777
Millie <millie.jackson1@gmail.com>
2022-03-23 12:02:40 +0000

    Initial scraper class based on Q+A video. Pre Selenium

--- DataCollectionPipeline/DataCollectionPipeline.py
+++ DataCollectionPipeline/DataCollectionPipeline.py
@@ -1,4 +1,14 @@
+#import numpy as np
 
-import numpy as np
+import scraper
+import time
 
-print("something")
+bot = scraper.Scraper('https://www.metasrc.com/5v5')
+bot.accept_cookies('Cookie Accept Path', 'iFrame Path') # Site doenst have cookies
+bot.click_search_bar('Search Bar Path')
+bot.write_in_search_bar('Hello', 'Path')
+time.sleep(2)
+
+print("End")
+
+
