bb7fcc94d742296e1d2ac3d274ec5304880439a9
Millie <millie.jackson1@gmail.com>
2022-03-23 12:18:46 +0000

    Update scraper.py

---
+++ DataCollectionPipeline/scraper.py
@@ -0,0 +1,42 @@
+from selenium.webdriver import chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+
+class Scraper:
+    def __init__(self, url, options=None):
+        self.url = url
+
+        if options:
+            self.driver = Chrome(ChromeDriverManager().install(), options=options)
+        else:
+            self.driver = Chrome(ChromeDriverManager().install())
+
+        self.driver.get(self.url)
+    
+    def accept_cookies(self, xpath, iframe=None):
+        try: # Incase there isnt a cookie button
+            time.sleep(10) # So the website can catch up to the bot
+
+            # If there is an iframe, remove it
+            if iframe: # iframe is the frame sometimes covering the 'Accept Cookies' button
+                self.driver.switch_to.frame(iframe)
+
+            cookies_button = self.driver.find_element(By.XPATH, xpath)
+            cookies_button.click()
+            time.sleep(5)
+        except:
+            print('No Cookie Button')
+
+    def click_search_bar(self, xpath):
+        search_bar = self.driver.find_element(By.XPATH, xpath)
+        search_bar.click()
+        return search_bar
+
+    def write_in_search_bar(self, text: str, xpath: str) -> None:
+        search_bar = self.click_search_bar(xpath)
+        search_bar.send_keys(text)
