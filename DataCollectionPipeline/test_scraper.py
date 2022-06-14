1d3163403b5542cdc6f4b560088586390aff6b0f
Millie <millie.jackson1@gmail.com>
2022-06-14 09:55:34 +0100

    Initial Unit Test Set Up
    Using unittest
    Creates an empty test for each class and function

---
+++ DataCollectionPipeline/test_scraper.py
@@ -0,0 +1,151 @@
+import unittest
+import scraper
+
+class scraperTestCase(unittest.TestCase):
+    
+    def test_dataClass(self):
+        pass
+    
+    def test_decoratorClass(self):
+        pass
+
+    def test_run(self):
+        pass
+
+    def test_cycleRecipeLinks(self):
+        pass
+
+    def test_getURL(self):
+        pass
+
+    def test_getTitle(self):
+        pass
+
+    def test_closeSession(self):
+        pass
+
+    def test_getSourceCode(self):
+        pass
+
+    def test_search(self):
+        pass
+
+    def test_searchbarTextAndClick(self):
+        pass
+
+    def test_findSearchbar(self):
+        pass
+
+    def test_home(self):
+        pass
+
+    def test_findRecipeList(self):
+        pass
+
+    def test_acceptCookies(self):
+        pass
+
+    def test_getRecipes(self):
+        pass
+
+    def test_getRecipeContainer(self):
+        pass
+
+    def test_makeRecipeList(self):
+        pass
+
+    def test_getPageURL(self):
+        pass
+
+    def test_getAllRecipePages(self):
+        pass
+
+    def test_getTotalPages(self):
+        pass
+
+    def test_getSearchList(self):
+        pass
+
+    def test_getUniqueID(self):
+        pass
+
+    def test_scrapeName(self):
+        pass
+
+    def test_scrapeTags(self):
+        pass
+
+    def test_scrapeDescription(self):
+        pass
+
+    def test_scrapeTotalTime(self):
+        pass
+
+    def test_scrapePrepTime(self):
+        pass
+
+    def test_scrapeCookTime(self):
+        pass
+
+    def test_scrapeAllergens(self):
+        pass
+
+    def test_scrapeAlternatives(self):
+        pass
+
+    def test_scrapeFreeFrom(self):
+        pass
+
+    def test_scrapeIngredients(self):
+        pass
+
+    def test_scrapeInstructions(self):
+        pass
+
+    def test_scrapeInstructions(self):
+        pass
+
+    def test_scrapeNotes(self):
+        pass
+
+    def test_scrapeStorage(self):
+        pass
+
+    def test_scrapeMainPhoto(self):
+        pass
+
+    def test_scrapeImages(self):
+        pass
+
+    def test_getRecipeDetails(self):
+        pass
+
+    def test_storeDetails(self):
+        pass
+
+    def test_jsonFile(self):
+        pass
+
+    def test_makeRaw_DataFolder(self):
+        pass
+
+    def jsonDump(self):
+        pass
+
+    def test_downloadImage(self):
+        pass
+
+    def test_makeImagesFolder(self):
+        pass
+
+    def test_makeRecipeFolder(self):
+        pass
+
+    def test_makeImage(self):
+        pass
+
+unittest.main(argv=['first-arg-is-ignored'], exit=False)
+
+#if __name__ == "__main__":
+#    test_scraper() 
+#    print("Passed")
